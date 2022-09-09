04a6485aa14ac327269330dbb4f25c7d38471e75
Evgeny Berg <evgeniy.berg@neuro-city.ru>
2022-09-09 15:11:30 +0300

    init

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import Layout from '../Layout/Layout';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.wrapper}>
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,11 @@
+import styles from './Layout.module.css';
+
+function Layout() {
+  return (
+    <div className={styles.wrapper}>
+      
+    </div>
+  );
+}
+
+export default Layout;
